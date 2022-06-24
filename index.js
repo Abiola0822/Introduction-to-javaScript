3f88a049eac798fa83a292ee9041a4cd48523f88
Abiola0822 <odusanyamistura76@gmail.com>
2022-06-24 15:29:56 +0100

    Adding files

---
+++ index.js
@@ -0,0 +1,15 @@
+let names = "Mistura Abiola";
+
+console.log(names);
+// prints: Mistura Abiola
+
+
+let height = "5'9"
+
+console.log(height);
+// prints: "5'9"
+
+let country = "Nigeria"
+
+console.log(country);
+// prints: "Nigeria"
